13b4f6f63fcfa4f310b5e0abdb5acc8a61857f71
Sooraj SR <soorajsr97@gmail.com>
2021-05-04 11:11:45 +0530

    Add files via upload

---
+++ DAY1/arith.dart
@@ -0,0 +1,10 @@
+void main()
+{
+int a=25;
+int b=2;
+double div=(a/b);
+print(a.toString()+ "+" +b.toString()+ "=" +div.toString());
+int sub=(b-a);
+print(sub);
+print(a.toString()+ "+" +b.toString()+ "=" +sub.toString());
+}
---
+++ DAY1/multisum.dart
@@ -0,0 +1,12 @@
+void main ()
+{
+int a=10;
+int b=5;
+int c=2;
+
+int d=(a*b);
+print(d);
+
+int e=(d+c);
+print(a.toString()+ "*" +b.toString()+ "+" +c.toString()+ "=" +e.toString());
+}
